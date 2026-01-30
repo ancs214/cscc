055dbe187e3d345a9228eaadcb668e7c80e09e48
ancs214 <ancs214@gmail.com>
2026-01-30 10:13:19 -0500

    function homework completed

--- scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-3-functions/homework/function-homework.py
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-3-functions/homework/function-homework.py
@@ -13,21 +13,20 @@ def guess_function(guess, result=4):
     """Function that compares two arguments
     (the user's guess vs actual result of the die roll)."""
     #error logic or "guard clause"
-    if guess < 1 or guess > 6:
+    if not isinstance(guess, int):
         print('Error! Guess must be a number between 1 and 6.')
         sys.exit()
-    elif not isinstance(guess, int):
+    elif guess < 1 or guess > 6:
         print('Error! Guess must be a number between 1 and 6.')
         sys.exit()
 
     #gameplay logic
     if guess == result:
-        print('matched')
-        sys.exit()
+        return 'matched'
     else:
-        print('did not match')
+        return 'did not match'
 
-#create variables for user guess and random number between 1 and 6
+#create variables for user guess and random number generated
 my_guess = 6
 die_throw = random_number()
 print(die_throw)
@@ -35,16 +34,6 @@ print(die_throw)
 #pass variables into our guess_function as arguments
 my_result = guess_function(my_guess, die_throw)
 print(f'I guessed {my_guess} and the die toss {my_result}.')
-
-# die_roll = random_number()
-#
-# while True:
-#     print('Guess the number between 1 and 6.')
-#     user_guess = (input())
-#
-#     guess_function(user_guess, die_roll)
-
-
 
 
 
