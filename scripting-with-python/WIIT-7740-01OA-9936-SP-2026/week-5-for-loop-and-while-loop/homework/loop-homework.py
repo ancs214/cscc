7fb4521de737134df62a270cf2241d6c78770709
ancs214 <ancs214@gmail.com>
2026-02-07 13:04:36 -0500

    created hw file for week 5

---
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-5-for-loop-and-while-loop/homework/loop-homework.py
@@ -0,0 +1,14 @@
+"""
+FILE_NAME: loop-homework.py
+AUTHOR: Ashley-Noel Swarn
+DATE:
+PURPOSE: Create a program to allow input of individual item prices until the user chooses to stop. program
+will place item prices in a list, then calculate sales tax and total bill amount.
+"""
+
+# Create a while loop that keeps the program running to allow input of individual item prices until the
+# user chooses to stop entering prices.
+# Requirement:
+# • Convert the string input provided by the user into an appropriate data type for calculating
+# numbers with decimal places.
+
