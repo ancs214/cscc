57298668e9edc20b2bbcf13736c90f40cfdd759a
ancs214 <ancs214@gmail.com>
2026-02-10 16:25:59 -0500

    loop hw almost complete. still troubleshooting final sums

--- scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-5-for-loop-and-while-loop/homework/loop-homework.py
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-5-for-loop-and-while-loop/homework/loop-homework.py
@@ -6,10 +6,10 @@ PURPOSE: Create a program to allow input of individual item prices until the use
 The program will add item prices to a list, then calculate sales tax and total bill amount.
 """
 
-import sys
 
 # start with empty list
 price_list = []
+
 # initial greeting
 print("Welcome to your own personal sales tax and bill calculator! Enter 'q' to end the program.")
 
@@ -24,17 +24,28 @@ while True:
         price_list.append(float(user_input))
     # if it results in ValueError, print error message
     except ValueError:
-        print("ERROR! \nEnter a number this time.")
+        print("ERROR! \nEnter a valid number this time.")
     else:
         print(f"${price_list[-1]} added.")
         print("Current price list: ", price_list)
 
-print("Here is your list of individual prices: ", price_list)
+def calculate_tax(original_prices, tax_rate = 0.0575):
+    tax_costs = []
 
-def calculate_tax(original_prices):
+    # for each element multiply by 0.0575 and add each tax cost to empty list
     for price in original_prices:
+        taxed_price = round((price * tax_rate), 2)
+        tax_costs.append(taxed_price)
+        tax_costs_sum = sum(tax_costs)
 
+    # using * (the unpacking operator) unpacks the list element to print without brackets
+    print('$', *tax_costs_sum)
 
+    # add sum of taxes and prices together to calculate total cost
+    total_price = round((sum(tax_costs) + sum(price_list)), 2)
+    print('$', total_price)
+
+# call function to calculate taxes and total cost
 calculate_tax(price_list)
 
 
