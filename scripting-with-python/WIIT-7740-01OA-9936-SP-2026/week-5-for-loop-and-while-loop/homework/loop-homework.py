09ece11d1d321b85c61c2ae470b17a2d2939bf02
ancs214 <ancs214@gmail.com>
2026-02-11 12:12:11 -0500

    completed loop hw

--- scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-5-for-loop-and-while-loop/homework/loop-homework.py
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-5-for-loop-and-while-loop/homework/loop-homework.py
@@ -1,49 +1,50 @@
 """
 FILE_NAME: loop-homework.py
 AUTHOR: Ashley-Noel Swarn
-DATE:
+DATE: 02-11-2026
 PURPOSE: Create a program to allow input of individual item prices until the user chooses to stop.
 The program will add item prices to a list, then calculate sales tax and total bill amount.
 """
-
 
 # start with empty list
 price_list = []
 
 # initial greeting
-print("Welcome to your own personal sales tax and bill calculator! Enter 'q' to end the program.")
+print("Welcome to your own personal sales tax and bill calculator! Enter 'q' to see final calculations.")
 
 while True:
-    # prompt user to enter item price and save to user_input variable
+    # prompt user to enter item price and save input to user_input variable
     user_input = input("Please enter an item price: ")
 
     if user_input == 'q':
         break
     try:
-        # try to add user_input value to item_list
+        # try to add user_input value to item_list as a float integer
         price_list.append(float(user_input))
     # if it results in ValueError, print error message
     except ValueError:
-        print("ERROR! \nEnter a valid number this time.")
+        print("ERROR! \nEnter a valid number.")
     else:
         print(f"${price_list[-1]} added.")
         print("Current price list: ", price_list)
 
 def calculate_tax(original_prices, tax_rate = 0.0575):
-    tax_costs = []
+    """Function to calculate total sales tax and bill amount"""
+    # initialize empty list
+    sales_tax = []
 
-    # for each element multiply by 0.0575 and add each tax cost to empty list
+    # for each element multiply by tax_rate and add each tax cost to empty list
     for price in original_prices:
-        taxed_price = round((price * tax_rate), 2)
-        tax_costs.append(taxed_price)
-        tax_costs_sum = sum(tax_costs)
+        taxed_price = (price * tax_rate)
+        sales_tax.append(taxed_price)
+        total_sales_tax = sum(sales_tax)
 
     # using * (the unpacking operator) unpacks the list element to print without brackets
-    print('$', *tax_costs_sum)
+    print('Total Sales Tax: $', round(total_sales_tax, 2))
 
     # add sum of taxes and prices together to calculate total cost
-    total_price = round((sum(tax_costs) + sum(price_list)), 2)
-    print('$', total_price)
+    total_price = round((sum(sales_tax) + sum(price_list)), 2)
+    print('Total Bill Amount: $', total_price)
 
 # call function to calculate taxes and total cost
 calculate_tax(price_list)
