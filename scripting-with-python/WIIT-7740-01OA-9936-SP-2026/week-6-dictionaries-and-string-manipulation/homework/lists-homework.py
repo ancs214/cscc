d20003d37311c7ed87475936df124fcac1d90896
Ashley-Noel <ancs214@gmail.com>
2026-02-17 05:12:05 -0500

    begin lists homework

---
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-6-dictionaries-and-string-manipulation/homework/lists-homework.py
@@ -0,0 +1,15 @@
+"""
+FILE_NAME: lists-homework.py
+AUTHOR: Ashley-Noel Swarn
+DATE: 02-17-2026
+PURPOSE: Create a function that takes a list of animals as input and outputs a dictionary with counts of each
+kind of animal from the list, then use methods to access specific elements.
+"""
+
+animal_list = ['lion', 'lion', 'boa', 'spider', 'spider']
+
+def create_dictionary(animals):
+    """takes list of animals and creates an indexed dictionary with count of each animal"""
+    dictionary = {}
+    try:
+        for animal in animals:
