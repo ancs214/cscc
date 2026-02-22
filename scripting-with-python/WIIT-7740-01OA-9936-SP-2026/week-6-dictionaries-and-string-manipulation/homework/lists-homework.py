d2f7d9d7c0e6f6c5043729b82ff12e12148d6cce
Ashley-Noel <ancs214@gmail.com>
2026-02-22 18:40:13 -0500

    added try except block. created another random animal list to work with

--- scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-6-dictionaries-and-string-manipulation/homework/lists-homework.py
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-6-dictionaries-and-string-manipulation/homework/lists-homework.py
@@ -7,15 +7,38 @@ kind of animal from the list, then use methods to access specific elements.
 """
 
 
-def create_dict(l):
+def create_dict(animals):
     """Function that takes a list as input and outputs a dictionary with counts of each unique animal"""
-    animal_dict = {}
-    for x in l:
-        if x in animal_dict:
-            animal_dict[x] += 1
-        else:
-            animal_dict[x] = 1
-    return print(animal_dict)
+    try:
+        animal_dict = {}
+        for animal in animals:
+            # if animal is already in dict, add one to it's corresponding value
+            if animal in animal_dict:
+                animal_dict[animal] += 1
+            # else create a key/value pair: key = animal, value = 1
+            else:
+                animal_dict[animal] = 1
 
-animals = ['lion', 'lion', 'boa', 'spider', 'spider']
-create_dict(animals)
+    except TypeError:
+        print('Oops! Enter a list into the function.')
+
+    # print(animal_dict)
+    return animal_dict
+
+
+animal_list = ['lion', 'lion', 'boa', 'spider', 'spider']
+create_dict(animal_list)
+
+# test error handling
+# create_dict(12345)
+
+random_animals_list = [
+    'lion', 'tiger', 'lion', 'bear', 'wolf', 'tiger', 'snake', 'lion', 'spider', 'bear',
+    'wolf', 'wolf', 'lion', 'spider', 'snake', 'panda', 'koala', 'panda', 'otter', 'koala',
+    'panda', 'falcon', 'otter', 'falcon', 'koala', 'panda', 'panda', 'otter', 'koala', 'falcon',
+    'otter', 'koala', 'panda', 'falcon', 'otter'
+]
+
+random_animals_dict = create_dict(random_animals_list)
+element_3 = random_animals_dict.keys()
+print(element_3)
