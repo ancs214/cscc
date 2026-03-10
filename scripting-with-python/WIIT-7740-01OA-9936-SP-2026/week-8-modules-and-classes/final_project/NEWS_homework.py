0c4c400631434d9c0384b5eb4af0bd2ba4a22461
Ashley-Noel <ancs214@gmail.com>
2026-03-10 06:02:42 -0400

    added ability to find top n words and print stats

--- scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-8-modules-and-classes/final_project/NEWS_homework.py
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-8-modules-and-classes/final_project/NEWS_homework.py
@@ -9,7 +9,7 @@ version locally.
 
 from pathlib import Path
 
-def count_words(path, ignore_words, search_word):
+def count_words(path, ignore_words, search_word, top_num):
     """
     Function that accepts a document and provides the following capabilities:
     Calculates total number of words, how many times each word appears and word's proportion,
@@ -33,7 +33,8 @@ def count_words(path, ignore_words, search_word):
 
     # find length of words list and display total number of words
     num_words = len(words)
-    print(f'The file {path} contains a total of {num_words} words.')
+    print('--- TOTAL WORDS ---')
+    print(f'The file {path} contains a total of {num_words} words.\n')
 
     counts = {}
     for word in words:
@@ -54,7 +55,7 @@ def count_words(path, ignore_words, search_word):
         # calculate proportion of word use:
         # using 'counts[key]' to look up each key's value, divide by total num of words
         word_proportion = round((counts[key] / num_words), 4)
-        # add entry to dictionary using word_proportion for values
+        # add key/value pair to proportion{}
         proportion[key] = word_proportion
 
     if search_word in proportion:
@@ -63,16 +64,33 @@ def count_words(path, ignore_words, search_word):
         # calculate percentage of proportion for readability
         percentage = (proportion[search_word] * 100)
         percentage_rounded = round(percentage, 4)
-        print(f'The word "{search_word}" appears in the text {word_repeats} times. \nProportion: {proportion[search_word]} or {percentage_rounded}%.')
+        print('--- PROPORTION OF SEARCHED WORD ---')
+        print(f'The word "{search_word}" appears in the text {word_repeats} times with a proportion of {proportion[search_word]} or {percentage_rounded}%.\n')
     else:
         print(f'The word "{search_word}" does not exist in the text.')
+
+    # sort counts dictionary and convert to list of pairs (each item is a tuple)
+    # (key=lambda tells python to look at the value as a number for sorting)
+    sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+    # get top n number of words
+    top_words_list = sorted_counts[:top_num]
+
+    print(f"--- Top {top_num} Most Frequent Words ---")
+    for item, count in top_words_list:
+        top_word = item
+        top_word_count = count
+        top_word_proportion = round((top_word_count / num_words), 4)
+        top_word_percentage = top_word_proportion * 100
+        top_word_percentage_rounded = round(top_word_percentage, 4)
+        print(f'Word: {top_word}\nAbsolute Count: {top_word_count}\nProportion: {top_word_proportion} or {top_word_percentage_rounded}%\n')
 
     return num_words, counts, proportion, word_proportion, word_repeats, percentage
 
 filename = 'dracula.txt'
 ignore = ["the", "a", "an", "and", "or", "but", "in", "on", "at", "to"]
-search_for_word = "i"
+search_for_word = "dear"
+top_n_words = 3
 # turns text string into a pathlib.Path object to pass to function:
 file_path = Path(filename)
-count_words(file_path, ignore, search_for_word)
+count_words(file_path, ignore, search_for_word, top_n_words)
 
