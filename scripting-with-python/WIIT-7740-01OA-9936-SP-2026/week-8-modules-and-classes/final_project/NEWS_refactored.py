cfd64b9476bf412354dd8ef57fc6f98e2f0cb82c
ancs214 <ancs214@gmail.com>
2026-03-11 17:31:56 -0400

    still refactoring news_refactored.py file. successfully done with text cleaning and word counts/proportion functions

--- scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-8-modules-and-classes/final_project/NEWS_refactored.py
+++ scripting-with-python/WIIT-7740-01OA-9936-SP-2026/week-8-modules-and-classes/final_project/NEWS_refactored.py
@@ -29,9 +29,6 @@ def clean_text(path):
 #------COUNT AND PROPORTION FUNCTION------
 #-----------------------------------------
 def count_proportion_func(words, ignore_words):
-    # total number of words
-    num_words = len(words)
-
     counts = {}
     for word in words:
         # ignore designated words input by user
@@ -46,45 +43,45 @@ def count_proportion_func(words, ignore_words):
             # if word is new, create key and set value to 1
             counts[word] = 1
 
+    # total number of non-ignored words
+    total_non_ignored = sum(counts.values())
+
     proportions = {}
     for word in counts:
         # look up each key's value, divide by total num of words
-        word_proportion = round((counts[word] / num_words), 4)
+        word_proportion = round((counts[word] / total_non_ignored), 4)
         # add key/value pair to proportion{}
         proportions[word] = word_proportion
 
-    return counts, proportions
+    return counts, proportions, total_non_ignored
 
 #------------------------------
 #------TOP WORDS FUNCTION------
 #------------------------------
-def top_words(top_num, count_dict, total_word_list):
-    # total count of words
-    total_count = len(total_word_list)
-
+def top_words(top_num, counts, num_words):
     # sort counts dictionary and convert to list of pairs (each item in list is a tuple)
     # (key=lambda tells python to look at the value as a number for sorting)
-    sorted_counts = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
 
     # get top n number of words
     top_words_list = sorted_counts[:top_num]
 
     top_results = []
     for top_word, count in top_words_list:
-        top_word_proportion = round((count / total_count), 4)
-        top_word_percentage = top_word_proportion * 100
+        top_word_proportion = round((count / num_words), 4)
+        top_word_percentage = round(top_word_proportion * 100, 4)
         top_results.append((top_word, count, top_word_proportion, top_word_percentage))
 
     return top_results
 
-def print_results(count_dict, total_words, prop, top, searched_word):
+def print_results(count_dict, total_count, prop, top, searched_word):
     print('--- TOTAL WORDS ---')
-    print(f'The file contains a total of {total_words} words.\n')
+    print(f'The file contains a total of {total_count} words.\n')
 
-    if searched_word in prop:
+    if searched_word in count_dict:
         word_repeats = count_dict[searched_word]
         proportion = prop[searched_word]
-        percentage = round(prop[searched_word] * 100, 4)
+        percentage = round(proportion * 100, 4)
 
         print('--- PROPORTION OF SEARCHED WORD ---')
         print(f'The word "{searched_word}" appears in the text {word_repeats} times with a proportion of {proportion} or {percentage}%.\n')
@@ -92,9 +89,9 @@ def print_results(count_dict, total_words, prop, top, searched_word):
         print(f'The word "{searched_word}" does not exist in the text.')
 
     print('--- TOP WORDS ---')
-    # tell python to grab one item from top list of tuples and split into four variables
-    for word, count, p, pct in top:
-        print(f'The word {word} appears {count} times with a proportion of {p} or {pct}%')
+    # "unpacking" tells python to grab one item from top list of tuples and split into four variables
+    for word, count, p, percent in top:
+        print(f'The word {word} appears {count} times with a proportion of {p} or {percent}%')
 
 def find_positions(word_list, search_word):
     positions = []
@@ -150,4 +147,19 @@ top_words_results = top_words(top_n_words, counted_words, cleaned_text)
 total_word_count = len(cleaned_text)
 print_results(counted_words, total_word_count, proportion_result, top_words_results, search_for_word)
 
-
+# filename = 'dracula.txt'
+# file_path = Path(filename)
+# ignore = ["the", "a", "an", "and", "or", "but", "in", "on", "at", "to"]
+#
+# # 1. Clean the text
+# all_words = clean_text(file_path)
+#
+# # 2. Get counts (this ignores the stop words)
+# counts, proportions = count_proportion_func(all_words, ignore)
+#
+# # 3. Get the top 10 results
+# # Note: We pass all_words here so the percentage is based on the full text
+# top_10 = top_words(10, counts, all_words)
+#
+# for word, count, prop, percent in top_10:
+#     print(f"{word}: {count} occurrences ({percent:.2f}%)")
